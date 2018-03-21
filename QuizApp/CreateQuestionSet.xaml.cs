71c24ee4c0bb3d71313455f8e8ede9d0b0ea3238
nnickelson <nnickelson@my.apsu.edu>
2018-03-20 20:46:12 -0500

    Working on CreateQuestionSet - added SubmitDeckName button Function

--- QuizApp/CreateQuestionSet.xaml.cs
+++ QuizApp/CreateQuestionSet.xaml.cs
@@ -11,7 +11,7 @@ using System.Windows.Shapes;
 
 using System.Web.Script.Serialization;
 using System.IO;
-
+using System;
 
 namespace QuizApp
 {
@@ -23,11 +23,44 @@ namespace QuizApp
         public CreateQuestionSet()
         {
             InitializeComponent();
+            Finishedbutton.Visibility.Equals("Hidden");
         }
 
         private void AddQuestionToDeck_Click(object sender, RoutedEventArgs e)
         {
 
         }
+
+        private void SubmitDeckName(object sender, RoutedEventArgs e)
+        {
+            if (DeckTitletextBox.Text == "")
+            {
+                throw new ArgumentException("Deck Name cannot be empty");
+            }
+
+            string filename = DeckTitletextBox.Text + ".json";
+            if (File.Exists(filename))
+            {
+                throw new ArgumentException("That filename exists");
+            }
+
+            this.toggleScreenButtons();
+
+            
+        }
+
+        private void toggleScreenButtons()
+        {
+            SubmitDeck.Visibility = Visibility.Hidden;
+            Finishedbutton.Visibility = Visibility.Visible;
+            QuestionNumBox.Visibility = Visibility.Visible;
+            QuestionTextBox.Visibility = Visibility.Visible;
+            AnswerTextBox.Visibility = Visibility.Visible;
+            QuestionNum.Visibility = Visibility.Visible;
+            QuestionText.Visibility = Visibility.Visible;
+            AnswerText.Visibility = Visibility.Visible;
+            AddQuestionToDeck.Visibility = Visibility.Visible;
+        }
+
     }
 }
