c05a19749879fa066ada31a2b845b7eb54a41c4b
Ron <Mr.Robot@DESKTOP-MD82PCD>
2018-03-25 15:07:10 -0500

    The ability to now study a deck has been implemented. Noticeable issuses:
    1) The term will slide out of the flashcard box if the page is stretched to far

--- QuizApp/StudyMyDeck.xaml.cs
+++ QuizApp/StudyMyDeck.xaml.cs
@@ -12,6 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Web.Script.Serialization;
+using System.IO;
+using Microsoft.Win32;
 
 namespace QuizApp
 {
@@ -20,9 +23,161 @@ namespace QuizApp
     /// </summary>
     public partial class StudyMyDeck : Page
     {
+        StudyDeck Deck = new StudyDeck();
+        string JSONflashcards;
+        int index = 0;
+        Flashcards f;
+        string fileName;
+        String path = "";
+        Boolean IsFrontShowing = true;
+
+        // Make the front of the flashcard visible
+         void makeFrontVisible()
+        {
+            // First, turn OFF the back
+            ImageViewer1.Visibility = Visibility.Hidden;
+            DefinitionBox.Visibility = Visibility.Hidden;
+
+            // Then turn ON the front
+           
+            TermBlock.Visibility = Visibility.Visible;
+            Previousbtn.Visibility = Visibility.Visible;           
+        }
+        // Make the back of the flashcard visible
+        void makeBackVisible()
+        {
+            // First, turn OFF the front
+            TermBlock.Visibility = Visibility.Hidden;
+
+            // Then turn ON the back
+            ImageViewer1.Visibility = Visibility.Visible;
+            DefinitionBox.Visibility = Visibility.Visible;
+        }
+        // Create serializer object to convert to and from the JSON format
+        JavaScriptSerializer ser = new JavaScriptSerializer();
         public StudyMyDeck()
         {
             InitializeComponent();
         }
+
+        private void SelectADeckbtn_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "JSON files (*.JSON)|*.JSON";
+            if (openFileDialog.ShowDialog() == true)
+            {
+                path = openFileDialog.FileName; // get file path
+                DeckTitlebox.Text= System.IO.Path.GetFileNameWithoutExtension(path); // get file name only
+                makeFrontVisible();
+
+                // Show flashcard border and buttons
+                Rectangle1.Visibility = Visibility.Visible;
+                Nextbtn.Visibility = Visibility.Visible;
+                Flipbtn.Visibility = Visibility.Visible;
+                Finishedbtn.Visibility = Visibility.Visible;
+
+            }
+
+
+            if (path != "")
+            {
+                JSONflashcards = File.ReadAllText(path);
+                Deck = ser.Deserialize<StudyDeck>(JSONflashcards);
+                TotalCardsBox.Text = (Deck.cards.Count).ToString();
+                f = Deck.cards[index];
+
+                TermBlock.Text = f.Front;
+                DefinitionBox.Text = f.Back;
+                CurrentCardTextbox.Text = (index + 1).ToString();
+
+                if (f.image != null)
+                {
+                    BitmapImage bitmap = new BitmapImage();
+                    bitmap.BeginInit();
+                    bitmap.UriSource = new Uri(f.image);
+                    bitmap.EndInit();
+                    ImageViewer1.Source = bitmap;
+                }
+                else
+                    ImageViewer1.Source = null;
+            }
+        }
+
+        private void Nextbtn_Click(object sender, RoutedEventArgs e)
+        {
+           
+            //  Note: Each time next is hit, always show front of the card and hide back 
+            if (index < Deck.cards.Count - 1)
+            {
+                index++;
+                f = Deck.cards[index];
+                TermBlock.Text = f.Front;
+                DefinitionBox.Text = f.Back;
+                CurrentCardTextbox.Text = (index + 1).ToString();
+
+                if (f.image != null)
+                {
+                    BitmapImage bitmap = new BitmapImage();
+                    bitmap.BeginInit();
+                    bitmap.UriSource = new Uri(f.image);
+                    bitmap.EndInit();
+                    ImageViewer1.Source = bitmap;
+                }
+                else
+                    ImageViewer1.Source = null;
+
+                makeFrontVisible();
+                IsFrontShowing = true;
+            }
+        }
+
+        private void Previousbtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (index != 0)
+            {
+                index--;
+                f = Deck.cards[index];
+                TermBlock.Text = f.Front;
+                DefinitionBox.Text = f.Back;
+                CurrentCardTextbox.Text = (index + 1).ToString();
+                if (f.image != null)
+                {
+                    BitmapImage bitmap = new BitmapImage();
+                    bitmap.BeginInit();
+                    bitmap.UriSource = new Uri(f.image);
+                    bitmap.EndInit();
+                    ImageViewer1.Source = bitmap;
+                }
+                else
+                {
+                    ImageViewer1.Source = null;
+                }
+
+                makeFrontVisible();
+                IsFrontShowing = true;
+            }
+        }
+
+        private void Finishedbtn_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(
+                    new Uri("/Home.xaml", UriKind.Relative));
+        }
+
+        private void Flipbtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (IsFrontShowing == false)
+            {
+                makeFrontVisible();
+                IsFrontShowing = true;
+            }
+            else
+            {
+                makeBackVisible();
+                IsFrontShowing = false;
+            }
+        }
     }
+
+
 }
