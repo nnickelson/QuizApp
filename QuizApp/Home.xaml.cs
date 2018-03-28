7a8c66859902763e3d27b8f974966667d1143737
Ron <Mr.Robot@DESKTOP-MD82PCD>
2018-03-28 18:08:12 -0500

    added subfolders for images, Study decks and Question. The root directory is called "Quiz App" and should appear on your desktop whenever you go to import a deck or create a deck.

--- QuizApp/Home.xaml.cs
+++ QuizApp/Home.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Web.Script.Serialization;
+using System.IO;
 
 namespace QuizApp
 {
@@ -33,6 +35,30 @@ namespace QuizApp
         private void StudyADeckbtn_Click(object sender, RoutedEventArgs e)
         {
             NavigationService.Navigate( new Uri("StudyMyDeck.xaml",UriKind.Relative));
+        }
+
+        private void ImportExportbtn_Click(object sender, RoutedEventArgs e)
+        {
+            // Create Root directory and subfolders if they do not exist yet
+            string RootPath, StudyDecksPath, ImagePath, QuestionsDeckPath;
+            //-------------- Create a root directory and subfolders on desktop if they haven't already been created yet------------
+            RootPath = StudyDecksPath = ImagePath = QuestionsDeckPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+
+            RootPath += @"\QuizApp\";
+            StudyDecksPath = RootPath + @"\StudyDecks\";
+            ImagePath = RootPath + @"\Images\";
+            QuestionsDeckPath = RootPath + @"\QuestionDecks\";
+            if (!Directory.Exists(RootPath))
+            {
+                Directory.CreateDirectory(RootPath);
+                Directory.CreateDirectory(StudyDecksPath);
+                Directory.CreateDirectory(ImagePath);
+                Directory.CreateDirectory(QuestionsDeckPath);
+            }
+            //---------------------------------------------------------------------------
+            NavigationService.Navigate(new Uri("Import.xaml", UriKind.Relative));
+
+
         }
     }
 }
